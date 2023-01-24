da7c3beae1aaf2602db232c5080e5da322a81554
Etienne Lopez <85631606+Deolinas@users.noreply.github.com>
2023-01-24 10:58:25 +0100

    Update member.test.js

--- API/tests/member.test.js
+++ API/tests/member.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('./../app');
 
 async function loginAdmin() {
-    const body = {mail:"touroncamille@icloud.com",password:"admin"};
+    const body = {mail:"touroncamille@icloud.com",password:"adminzzzzzzzzz"};
     const resp = await request(app)
         .post("/login")
         .send(body);    
